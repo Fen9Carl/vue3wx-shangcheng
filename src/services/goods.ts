4e6c17efa582bc7d0edf4dd2d9e1c4a99f795263
happyuser2058 <2058277327@qq.com>
2025-11-08 20:47:39 +0800

    商品详情页面渲染

--- src/services/goods.ts
+++ src/services/goods.ts
@@ -1,8 +1,9 @@
 import { http } from '@/utils/http'
+import type { GoodsResult } from '@/types/goods'
 
 //获取商品详情数据接口
 export const getGoodsByIdAPI = (id: string) => {
-  return http({
+  return http<GoodsResult>({
     method: 'GET',
     url: '/goods',
     data: {
