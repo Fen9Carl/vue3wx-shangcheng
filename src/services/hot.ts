2d152b72f5f0e12439a0ff67bb704d15a6d2ad28
happyuser2058 <2058277327@qq.com>
2025-11-08 11:16:50 +0800

    热门推荐类型定义

--- src/services/hot.ts
+++ src/services/hot.ts
@@ -1,10 +1,11 @@
 import type { PageParams } from '@/types/global'
+import type { HotResult } from '@/types/hot'
 import { http } from '@/utils/http'
 
 type HotParams = PageParams & { subType?: string }
 // 通用的热门推荐数据类型
 export const getHotRecommendAPI = (url: string, data?: HotParams) => {
-  return http({
+  return http<HotResult>({
     method: 'GET',
     url,
   })
