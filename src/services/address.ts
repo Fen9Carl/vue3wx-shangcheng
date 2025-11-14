c268bf319613805bfad7a881d3d8e35211151f2d
happyuser2058 <2058277327@qq.com>
2025-11-14 12:56:59 +0800

    地址管理页面渲染

--- src/services/address.ts
+++ src/services/address.ts
@@ -1,11 +1,20 @@
 import { http } from '@/utils/http'
 import type { AddressParams } from '@/types/address'
+import type { AddressItem } from '@/types/address'
 
-//收货地址接口
+//添加收货地址接口
 export const postMemberAddressAPI = (data: AddressParams) => {
   return http({
     method: 'POST',
     url: '/member/address',
     data,
+  })
+}
+
+//获取收获地址接口
+export const getMemberAddressAPI = () => {
+  return http<AddressItem[]>({
+    method: 'GET',
+    url: '/member/address',
   })
 }
